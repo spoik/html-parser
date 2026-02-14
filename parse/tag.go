b320e699589135d4cbd4e0ea96b4949c3359e9e3
spike <sebfrye@gmail.com>
2026-02-14 13:25:48 -0600

    Moved tag type parsing to it's own file

--- parse/tag.go
+++ parse/tag.go
@@ -3,11 +3,8 @@ package parse
 import (
 	"bufio"
 	"errors"
-	"fmt"
 	"io"
-	"iter"
 	"slices"
-	"strings"
 
 	"github.com/spoik/html-parser/html"
 )
@@ -43,59 +40,6 @@ func ParseTag(r *bufio.Reader) (*html.Tag, error) {
 	}
 
 	return tag, nil
-}
-
-func getTagType(r *bufio.Reader) (string, error) {
-	var typeBuilder strings.Builder
-
-	nextByte, checkErr := tagTypeIterator(r)
-
-	for byte := range nextByte {
-		typeBuilder.WriteByte(byte)
-	}
-
-	if err := checkErr(); err != nil {
-		return "", err
-	}
-
-	if typeBuilder.Len() == 0 {
-		return "", fmt.Errorf("Unable to find tag.")
-	}
-
-	return typeBuilder.String(), nil
-}
-
-func tagTypeIterator(r *bufio.Reader) (iter.Seq[byte], func() error) {
-	var err error
-
-	seq := func(yield func(byte) bool) {
-		for {
-			bytes, e := r.Peek(1)
-
-			if e != nil {
-				if errors.Is(e, io.EOF) {
-					break
-				}
-
-				err = e
-				break
-			}
-
-			byte := bytes[0]
-
-			if isTagEndChar(byte) || isAttributeDeliminer(byte) {
-				break
-			}
-
-			r.Discard(1)
-
-			if !yield(byte) {
-				break
-			}
-		}
-	}
-
-	return seq, func() error { return err }
 }
 
 func getAttributes(r *bufio.Reader) ([]*html.Attribute, error) {
