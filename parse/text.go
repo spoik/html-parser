8da9e42e5a3e6ee0681b72520395f5bd2ed565d7
Spike Frye <sebfrye@gmail.com>
2026-02-14 22:38:37 -0600

    Updated seekToText to use recursion instead of a for loop

--- parse/text.go
+++ parse/text.go
@@ -25,42 +25,40 @@ func getText(r *bufio.Reader) (string, error) {
 }
 
 func seekToText(r *bufio.Reader) error {
-	for {
-		bytes, err := r.Peek(1)
+	bytes, err := r.Peek(1)
 
-		if errors.Is(err, io.EOF) {
-			return errNoText
-		}
-
-		if err != nil {
-			return err
-		}
-
-		b := bytes[0]
-
-		if !isTagEndChar(b) {
-			_, err := r.Discard(1)
-
-			if err != nil {
-				return err
-			}
-
-			continue
-		}
-
-		// If this is a self closing tag, there is no text to extract.
-		if b == '/' {
-			return errNoText
-		}
-
-		_, err = r.Discard(1)
-
-		if err != nil {
-			return err
-		}
-
-		return nil
+	if errors.Is(err, io.EOF) {
+		return errNoText
 	}
+
+	if err != nil {
+		return err
+	}
+
+	b := bytes[0]
+
+	if !isTagEndChar(b) {
+		_, err := r.Discard(1)
+
+		if err != nil {
+			return err
+		}
+
+		return seekToText(r)
+	}
+
+	// If this is a self closing tag, there is no text to extract.
+	if b == '/' {
+		return errNoText
+	}
+
+	_, err = r.Discard(1)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func readText(r *bufio.Reader) (string, error) {
